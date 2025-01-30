0e070ff2991b5b964c033fa7f3e95a67e5805038
Mela <hollynwangwa@gmail.com>
2025-01-30 09:37:06 -0600

    Add gtm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import WelcomeCard from "./components/WelcomeCard/WelcomeCard";
+import React, { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import "./App.css";
+import WelcomeCard from "./components/WelcomeCard/WelcomeCard";
 import WhatsInside from "./components/WhatsInside/WhatsInside";
 import YourStories from "./components/YourStories/YourStories";
 import Footer from "./components/Footer/Footer";
+
 const App = () => {
-    return (
-      <>
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'G-TY2GQSJX16'
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
+  return (
+    <>
       <WelcomeCard />
       <WhatsInside />
       <YourStories />
       <Footer />
-      </>
-    );
+    </>
+  );
 };
 
 export default App;
