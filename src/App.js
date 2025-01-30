e5711670d86395e7b098d5ca1dc78b28522d404b
Mela <hollynwangwa@gmail.com>
2025-01-30 10:40:44 -0600

    Fix import statement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
 import "./App.css";
 import WelcomeCard from "./components/WelcomeCard/WelcomeCard";
