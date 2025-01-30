69bd30a8eeb53cac2bd76341d391e403f578093a
Mela <hollynwangwa@gmail.com>
2025-01-30 10:27:35 -0600

    Change up page description slightly

--- src/components/WhatsInside/WhatsInside.js
+++ src/components/WhatsInside/WhatsInside.js
@@ -15,22 +15,26 @@ const WhatsInside = () => {
         {
             className: WhatsInsideStyles.twoPages,
             img: JournalPage,
-            text: "Journal Pages: Write down the stories you'll be telling for years to come, like those iconic memories that start with 'Remember when...?', the moments that you can't think about without bursting into laughs, or the tender memories that you'll carry forever in your heart (and this book!).",
+            title: "Journal Pages",
+            text: "Write down the stories you'll be telling for years to come, like those iconic memories that start with 'Remember when...?', the moments that you can't think about without bursting into laughs, or the tender memories that you'll carry forever in your heart (and this book!).",
         },
         {
             className: WhatsInsideStyles.onePage,
             img: FriendPage,
-            text: "Friend Profiles: Because every great story has great characters-dedicate a page to the friends who made the moments so special.",
+            title: "Friend Profiles",
+            text: "Because every great story has great characters-dedicate a page to the friends who made the moments so special.",
         },
         {
             className: WhatsInsideStyles.onePage,
             img: PhotoPage,
-            text:  "Little Moments, Big Memories: This is the perfect spot for those quick, unforgettable moments! Jot down your mini memories, add a photo and let the story shine.",
+            title: "Little Moments, Big Memories",
+            text: "This is the perfect spot for those quick, unforgettable moments! Jot down your mini memories, add a photo and let the story shine.",
         },
         {
             className: WhatsInsideStyles.onePage,
             img: IndexPage,
-            text: "Index Pages: A custom table of contents for your stories. Add page numbers and titles as you go so you can flip right to the good stuff later.",
+            title: "Index Pages",
+            text: "A custom table of contents for your stories. Add page numbers and titles as you go so you can flip right to the good stuff later.",
         }
     ]
 
@@ -42,8 +46,12 @@ const WhatsInside = () => {
                     return (
                         <SwiperSlide key={index}>
                             <div className={WhatsInsideStyles.pageContent}>
-                            <img src={page.img} className={page.className} alt='book page sample' />
-                                <p>{page.text}</p>
+                                <img src={page.img} className={page.className} alt='book page sample' />
+                                <div className={WhatsInsideStyles.pageText}>
+                                    <h4>{page.title}</h4>
+                                    <p>{page.text}</p>
+                                </div>
+
                             </div>
                         </SwiperSlide>
                     )
